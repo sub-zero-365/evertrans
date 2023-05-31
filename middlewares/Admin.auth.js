c9e7eb630b1507bef6c51553b2a25efac6c2d8f3
mmanuel <bateemma14@gmail.com>
2023-05-31 01:25:52 +0100

    added some security change

--- middlewares/Admin.auth.js
+++ middlewares/Admin.auth.js
@@ -1,21 +1,26 @@
-const { BadRequestError } = require("../error")
+const { BadRequestError } = require("../error");
+const Admin = require("../models/Admin.js")
 const jwt = require("jsonwebtoken")
-const auth = async(req, _, next) => {
+const auth = async (req, _, next) => {
     const authHeader = req.headers.authorization;
     if (!authHeader) {
-        throw BadRequestError("please provide an auth header")
-
+        throw BadRequestError("please provide an auth header");
     }
     if (!authHeader.startsWith(process.env.jwtSecret)) {
         throw BadRequestError("please provide a valid auth  header")
-
     }
     const token = authHeader.split(" ")[1];
-
     try {
-        jwt.verify(token, process.env.jwtSecret, );
+        const payload = jwt.verify(token, process.env.jwtSecret,);
+        const isAdmin = await Admin.findOne({
+            _id: payload._id
+        })
+        if (!isAdmin) {
+            throw new Error("something went wrong");
+        }
         req.user = true
         next()
+
     } catch (err) {
         throw BadRequestError("bad token")
     }
