5e96932cba37bcc56b896b6d23cdd4fb4601076f
mmanuel <bateemma14@gmail.com>
2023-06-28 08:34:09 +0200

    added block option

--- middlewares/Auth.User.js
+++ middlewares/Auth.User.js
@@ -1,29 +1,39 @@
-const { BadRequestError } = require("../error")
-const jwt = require("jsonwebtoken")
-const Auth =async(req,res,next)=>{
+const {
+    BadRequestError,
+    DisableRequestError,
+    UnethenticatedError,
+    UnathorizedError } = require("../error")
+const jwt = require("jsonwebtoken");
+const Restricted = require("../models/RestrictedUsers");
+const isUserNotRestricted = async (user_id) => {
+    return new Promise(async function (resolve, reject) {
+        const isuserblocked = await Restricted.findOne({ user_id });
+        if (isuserblocked) return reject(DisableRequestError("cant perform this action casuse user is temporal block by admin contact admin"))
+        return resolve("everything ok");
+    })
+}
+const Auth = async (req, res, next) => {
     const authHeader = req.headers.authorization;
-    // console.log(req.headers)
     if (!authHeader) {
-        throw BadRequestError("please provide an auth header")
-
+        throw UnathorizedError("please provide an auth header")
     }
     if (!authHeader.startsWith(process.env.jwtSecret)) {
-        throw BadRequestError("please provide a valid auth  header")
-
+        throw UnathorizedError("please provide a valid auth  header")
     }
     const token = authHeader.split(" ")[1];
-
     try {
-       const payload= jwt.verify(token, process.env.jwtSecret);
-        req.userInfo ={
-        _id:payload._id,
-        phone:payload.phone
+        const payload = jwt.verify(token, process.env.jwtSecret);
+        req.userInfo = {
+            _id: payload._id,
+            phone: payload.phone
         }
+        await isUserNotRestricted(payload._id);
         next()
     } catch (err) {
-        throw BadRequestError("bad token")
+        console.log(err)
+        throw err
     }
 
 }
 
-module.exports=Auth
+module.exports = Auth
