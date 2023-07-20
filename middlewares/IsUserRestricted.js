4f753c61fb8d9994495fda917bdf2773a774fee3
mmanuel <bateemma14@gmail.com>
2023-07-20 14:30:05 +0100

    added new block user route

--- middlewares/IsUserRestricted.js
+++ middlewares/IsUserRestricted.js
@@ -1,12 +1,16 @@
 const {
     DisableRequestError
-    } = require("../error")
+} = require("../error")
 const Restricted = require("../models/RestrictedUsers");
 const isUserNotRestricted = async (req, res, next) => {
     const user_id = req?.userInfo?._id;
     return new Promise(async function (resolve, reject) {
         const isuserblocked = await Restricted.findOne({ user_id });
-        if (isuserblocked) { return reject(DisableRequestError("cant perform this action casuse user is temporal block by admin contact admin")) }
+        if (isuserblocked) {
+            return reject(DisableRequestError(`
+       your account has been block contact admin  for information
+        `))
+        }
         else {
             next()
             resolve("everything ok");
