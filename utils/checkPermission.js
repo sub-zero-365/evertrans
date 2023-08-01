5c9e67ac1e7f329c6bca3f63c4654177c51cdb4c
mmanuel <bateemma14@gmail.com>
2023-08-01 14:10:11 +0100

    added some code for security

--- utils/checkPermission.js
+++ utils/checkPermission.js
@@ -4,6 +4,7 @@ const checkPermissions = async (requestUser) => {
   let user = await Assistant.findOne({
     _id: requestUser
   })
+  console.log("this is the user ", user)
   if (!user) throw BadRequestError("user not permited to make this actions,please login as assistant ")
   return user._id
 }
