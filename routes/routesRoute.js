1a4832a9a6b30c4bb4ce2cd69a83b6460cc9f482
mmanuel <bateemma14@gmail.com>
2023-10-25 09:00:30 +0100

    added get price route

--- routes/routesRoute.js
+++ routes/routesRoute.js
@@ -5,12 +5,15 @@ const {
     updateRoutes,
     getAllRoutes,
     deleteRoutes,
-    getStaticRoute
+    getStaticRoute,
+    getRoute
 } = require("../controllers/RouteController")
 router.route("/new").post(admin_auth,
     createRoutes)
 router.route("/").get(getAllRoutes)
 router.route("/:id")
     .delete(admin_auth, deleteRoutes)
+router.route("/get")
+.get(getRoute)
 
 module.exports = router
