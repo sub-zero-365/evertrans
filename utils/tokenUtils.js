226f7689495a00b45b22ceb7184fd7ee32c074ac
mmanuel <bateemma14@gmail.com>
2024-01-08 14:10:00 +0100

    fixed the code in next update

--- utils/tokenUtils.js
+++ utils/tokenUtils.js
@@ -1,8 +1,7 @@
-// import jwt from 'jsonwebtoken';
 const jwt = require("jsonwebtoken")
 
 const createJWT = (payload, secret = process.env.jwtSecret) => {
-  const token = jwt.sign(payload, (secret || process.env.jwtSecret), {
+  const token = jwt.sign(payload, process.env.jwtSecret, {
     expiresIn: "10d",
   });
   return token;
