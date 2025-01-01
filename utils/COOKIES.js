4aa56773f5591788175ab3427678d84162cee8e7
Ako Bate Emmanuel <95502815+Abate-Mony@users.noreply.github.com>
2025-01-01 19:16:40 +0000

    testing again here

--- utils/COOKIES.js
+++ utils/COOKIES.js
@@ -1,17 +1,16 @@
 const isProduction = process.env.NODE_ENV == "production"
 // this set cookies to the browser when depending on the environment varaible
 
-  const cookies = (time=null) => {
-    const obj = {
-      httpOnly: true,
-      expires: time ? new Date(Date.now() + time) : new Date(Date.now()),
-      secure: isProduction,
-    };
-    if (isProduction) obj.sameSite = "none";
-  
-    return {
-      ...obj,
-    };
+const cookies = (time = null) => {
+  const obj = {
+      httpOnly: true, // Prevents client-side access to the cookie
+      secure: isProduction, // Ensures cookies are sent only over HTTPS in production
+      sameSite: isProduction?'None':"lax",  // Allows the cookie to be sent across subdomains
+      expires: time ? new Date(Date.now() + time) : new Date(0), // Sets expiry time or invalidates the cookie
   };
 
+  return { ...obj };
+};
+
+  
 module.exports=cookies
